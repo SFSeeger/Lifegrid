2c49effe88d85984061366d9aa175f5048886924
Simon Felix Seeger <simon.f.seeger@gmx.de>
2021-10-15 16:40:00 +0200

    Multi Kernel Lenia
    Signed-off-by: Simon Felix Seeger <simon.f.seeger@gmx.de>

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.lib.type_check import asfarray
 import scipy
-from PIL import Image, ImageDraw
-from simulation.simulation import Simulation, Simulation_Helper
+import cv2
+from simulation.simulation import *
 from simulation.creatures import creatures
+from simulation.utils import *
+from simulation.utils import Renderer
 
 sh = Simulation_Helper()
+renderer = Renderer('out/')
 
 size = 128
 scale = 1
 cx, cy = 20, 20
+mid = size // 2
 
-orbitum = creatures['Orbitum']
+orbitum = creatures['geminium']
 radius = orbitum['R']
 time_step = orbitum['T']
+orbitum['b'] = np.asarray(orbitum['b'])
 
 C = np.asarray(orbitum['cells'])
 ''' create empty world '''
-# A = np.random.rand(size, size)
 A = np.zeros([size, size])
 ''' place scaled pattern '''
 C = scipy.ndimage.zoom(C, scale, order=0)
 radius *= scale
 A[cx : cx + C.shape[0], cy : cy + C.shape[1]] = C
-
-# radius = 10
-# time_step = 10
 kernel_form = (
-    np.linalg.norm(np.asarray(np.ogrid[-radius:radius, -radius:radius]) + 1)
+    np.linalg.norm(np.asarray(np.ogrid[-mid:mid, -mid:mid], dtype=object) + 1)
     / radius
+    * len(orbitum['b'])
 )
-
-k = (kernel_form < 1) * sh.generate_bell(kernel_form, 0.5, 0.15)
+plt.imsave(
+    'tmp.png',
+    (kernel_form < len(orbitum['b']))
+    * orbitum['b'][np.minimum(kernel_form.astype(int), len(orbitum['b']) - 1)]
+    * sh.generate_bell(kernel_form % 1, 0.5, 0.15),
+)
+# k = (kernel_form < len(orbitum['b'])) * sh.generate_bell(kernel_form, 0.5, 0.15)
+k = (
+    (kernel_form < len(orbitum['b']))
+    * orbitum['b'][np.minimum(kernel_form.astype(int), len(orbitum['b']) - 1)]
+    * sh.generate_bell(kernel_form % 1, 0.5, 0.15)
+)
+k = sh.trim(k)
 k = k / np.sum(k)
 
 s = Simulation(k, time_step, orbitum['m'], orbitum['s'])
-# plt.imsave('kernel.png', k)
-
-plt.imsave(f'out/{0}.png', A)
-for i in range(1, 100):
-    ss = s.smooth_step(A)
-    plt.imsave(f'out/{i}.png', ss)
-    A = ss
+renderer.step_to_image(A, s)
+renderer.images_to_video('out.avi', 18)
--- simulation/simulation.py
+++ simulation/simulation.py
@@ -9,14 +9,32 @@ class Simulation_Helper:
         pass
 
     def generate_bell(self, x, m, s):
+        """
+        m: x-axis shift
+        s: y-axis spread
+        """
         bell = lambda x, m, s: np.exp(-(((x - m) / s) ** 2) / 2)
         return bell(x, m, s)
 
     def generate_conway_kernel(self):
         return np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
+    def trim(self, K):
+        mask = K == 0
+        rows = np.flatnonzero((~mask).sum(axis=1))
+        cols = np.flatnonzero((~mask).sum(axis=0))
+        return K[rows.min() : rows.max() + 1, cols.min() : cols.max() + 1]
+
 
 class Simulation:
+    """
+    Parameter:
+    K: Kernel
+    T: dt
+    m: growth function x-axis shift
+    s: growth function y-axis spread
+    """
+
     def __init__(self, K, T, m, s) -> None:
         self.K = K
         self.T = T
@@ -30,19 +48,20 @@ class Simulation:
     def apply_kernel(self, A):
         return signal.convolve2d(A, self.K, mode='same', boundary='wrap')
 
-    def growth_function(self, A, U):
-        return np.logical_or(np.logical_and(A, (U == 2)), (U == 3))
-
-    def smooth_growth_function(self, U):
-        return self.sh.generate_bell(U, self.m, self.s) * 2 - 1
-
     def apply_growth(self, A, U):
         return self.growth_function(A, U)
+
+    def growth_function(self, A, U):
+        return np.logical_or(np.logical_and(A, (U == 2)), (U == 3))
 
     def step(self, A):
         return np.clip(
             self.apply_growth(A, self.apply_kernel(A, self.K)), 0, 1
         )
+
+    # smooth
+    def smooth_growth_function(self, U):
+        return self.sh.generate_bell(U, self.m, self.s) * 2 - 1
 
     def smooth_step(self, A):
         return np.clip(
